d4efebc9f43192af1a13ed00b3d4d495a8adda1d
Jon Allen <jayayedev@gmail.com>
2025-10-17 15:30:53 +0100

    update main profile picture

---
+++ src/features/Images/GlosPoliceBand_20251011.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+import GlosPoliceBandSrc from "@/images/glospoliceband_cheltenham_20251011.jpg";
+
+export const GlosPoliceBand_20251011 = () => (
+  <Image
+    src={GlosPoliceBandSrc}
+    alt="The Gloucestershire Police Band, charity concert at St Philip and St James in Cheltenam 11/10/2025"
+    width={5450}
+    height={12611859}
+  />
+);
--- src/features/Images/index.ts
+++ src/features/Images/index.ts
@@ -1,3 +1,4 @@
 export { CdCover } from "./CdCover";
 export { FindUsOnFacebook } from "./FindUsOnFacebook";
 export { GlosPoliceBand_20161114 } from "./GlosPoliceBand_20161114";
+export { GlosPoliceBand_20251011 } from "./GlosPoliceBand_20251011";
