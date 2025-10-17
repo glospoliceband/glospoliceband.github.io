f4f8b89fe6d853e5392ae0acd651199fe7dcfa45
Jon Allen <jayayedev@gmail.com>
2025-10-17 15:30:53 +0100

    update main profile picture

--- src/features/MainPage/MainPage.tsx
+++ src/features/MainPage/MainPage.tsx
@@ -1,4 +1,4 @@
-import { GlosPoliceBand_20161114 } from "@/features/Images";
+import { GlosPoliceBand_20251011 } from "@/features/Images";
 
 import { MainPageIntro } from "./MainPageIntro";
 import { MainPageText } from "./MainPageText";
@@ -7,7 +7,7 @@ export const MainPage = () => (
   <>
     <h1>Gloucestershire Police Band</h1>
     <MainPageIntro />
-    <GlosPoliceBand_20161114 />
+    <GlosPoliceBand_20251011 />
     <MainPageText />
   </>
 );
