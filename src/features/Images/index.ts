16295eb8507f4adc1c3a19eb8de7f3a293b1f398
Jon Allen <jayayedev@gmail.com>
2026-07-26 11:58:12 +0100

    add knip and update dependencies

--- src/features/Images/index.ts
+++ src/features/Images/index.ts
@@ -1,4 +1,3 @@
 export { CdCover } from "./CdCover";
 export { FindUsOnFacebook } from "./FindUsOnFacebook";
-export { GlosPoliceBand20161114 } from "./GlosPoliceBand20161114";
 export { GlosPoliceBand20251011 } from "./GlosPoliceBand20251011";
